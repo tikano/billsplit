619797ef8a734fa5b0b6c72d2abb5181b4a71a4c
Chris <chrisvarghese2000@gmail.com>
2023-04-10 11:27:19 -0500

    added wireframes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from receipt_ocr import get_receipt_text
+from text_dection import detect_text
 
 file_name = '/Users/chrisv/Desktop/CS 222/course-project-team95-billsplit/sample_receipt_1.jpg'
 text = get_receipt_text(file_name)
 print(text)
+
+#detect_text(file_name)
+
