61710a85f132775ba7ad45d039d2a787deb46c79
Chris <chrisvarghese2000@gmail.com>
2023-04-17 14:37:25 -0500

    setting up flask

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from receipt_ocr import get_receipt_text
 from text_dection import detect_text
+from flask import Flask
+
+app = Flask(__name__)
 
 file_name = '/Users/chrisv/Desktop/CS 222/course-project-team95-billsplit/sample_receipt_1.jpg'
-text = get_receipt_text(file_name)
-print(text)
+
+@app.route('/')
+def main():
+    text = get_receipt_text(file_name)
+    return text
+
+#text = get_receipt_text(file_name)
+#print(text)
 
 #detect_text(file_name)
 
