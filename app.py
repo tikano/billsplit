554df6a2e6b3b4bf2da4c0e725235f313bd8f23c
Chris <chrisvarghese2000@gmail.com>
2023-05-02 15:18:40 -0500

    creating basic frontend

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from receipt_ocr import get_receipt_text
 from text_dection import detect_text
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -15,4 +15,23 @@ def main():
 #print(text)
 
 #detect_text(file_name)
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    # Check if an image was uploaded
+    if 'image' not in request.files:
+        return 'No image file found', 400
+
+    # Get the uploaded image file
+    file = request.files['image']
+
+    # TODO: Use Google OCR library to process the image
+    text = get_receipt_text(file)
+    return text, 200
+
+    # Return a success response
+    # return 'Image uploaded successfully', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
