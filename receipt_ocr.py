d3788d6011d16a892015b60cb248c0ca1aef9e68
Om Desai <oadmathgenie@gmail.com>
2023-05-04 09:19:11 -0400

    Rewrote the ocr using keras-ocr on this branch - potentially less accurate but easier to use and doesn't want your credit card

--- receipt_ocr.py
+++ receipt_ocr.py
@@ -1,9 +1,17 @@
 import io
 import os
 from google.cloud import vision
+from PIL import Image;
+import matplotlib.pyplot as plt
+import numpy as np;
+
+import keras_ocr;
+
+model = keras_ocr.pipeline.Pipeline();
+
 
 def get_text_from_file(file):
-
+    """
     client = vision.ImageAnnotatorClient()
 
     content = file.read()
@@ -13,6 +21,23 @@ def get_text_from_file(file):
     text = response.text_annotations[0].description
 
     return text
+    """
+    newimage = file;
+    
+    
+    filearray = [newimage]
+    prediction = model.recognize(filearray)
+    print(prediction)
+    
+    filearray = [np.asarray(Image.open(file))];
+    fig, axs = plt.subplots(nrows=len(filearray), figsize=(20, 20))
+    axs = [axs]
+    for ax, img, predictions in zip(axs, filearray, prediction):
+        keras_ocr.tools.drawAnnotations(image=img, predictions=predictions, ax=ax)
+    fig.savefig("plot.png")
+    text = "n"
+    return text
+    
 
 def get_receipt_text(file_name):
     """Extracts text from a receipt image using Google Cloud OCR API.
